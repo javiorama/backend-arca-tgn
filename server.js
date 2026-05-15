bc4e5c324bfd1ef2e0afbd81f0c322b9453c5c94
javiorama <249666811+javiorama@users.noreply.github.com>
2026-05-15 14:08:47 -0300

    Update Afip module import path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const Afip = require('afip.js');
+const Afip = require('@afipsdk/afip.js');
 const fs = require('fs');
 const path = require('path');
 
